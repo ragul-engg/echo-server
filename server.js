6d516deb9f7d301535e47dd98776d795b4b5bc4a
raguls-sahaj <raguls@sahaj.ai>
2024-11-06 14:52:01 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express()
+
+const PORT = 9000
+
+app.use(express.json())
+
+app.get("/echo", (req, res) => {
+    res.send(req)
+})
+
+app.put("/echo", (req, res) => {
+	res.send(req.body)
+})
+
+app.post("/echo", (req, res) => {
+    console.log(req.body)
+	res.send(req.body)
+})
+
+app.listen(PORT, () => {
+	console.log(`server is started in port ${PORT}`)
+})
